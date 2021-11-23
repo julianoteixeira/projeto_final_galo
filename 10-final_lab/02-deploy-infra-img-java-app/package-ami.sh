7bebdbdb7ca7a67fa51801a31cf9f349d34f3960
guiagb <guilherme.agb94@gmail.com>
2021-11-23 19:32:42 -0300

    subindo alteracoes do docker

--- 10-final_lab/02-deploy-infra-img-java-app/package-ami.sh
+++ 10-final_lab/02-deploy-infra-img-java-app/package-ami.sh
@@ -5,6 +5,6 @@ VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 cd 10-final_lab/02-deploy-infra-img-java-app/terraform
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
-cd ../../10-final_lab/02-deploy-infra-img-java-app/terraform-ami
+cd ../terraform-ami
 terraform init
 TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
