6880b47b4bfd6a14824fd9ba902cc886a5c21ac5
guiagb <guilherme.agb94@gmail.com>
2021-11-23 14:12:14 -0300

    subindo pro juliano

--- 01-terraform_lab_exemplos/11-resource_security_group-01/main.tf
+++ 01-terraform_lab_exemplos/11-resource_security_group-01/main.tf
@@ -1,65 +1,40 @@
 provider "aws" {
   region = "sa-east-1"
 }
-resource "aws_instance" "ec2-mamede-terraform-01" {
-  subnet_id     = aws_subnet.sub-mamede-terraform-1a.id
-  ami= "ami-0e66f5495b4efdd0f"
-  instance_type = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids = ["${aws_security_group.sg-mamede-terraform-01.id}"]
-  key_name="itau-treinamento"
-  root_block_device {
-    encrypted = true
-    volume_size = 8
-  }
-  tags = {
-    Name = "ec2-mamede-terraform-01"
-  }
-}
-resource "aws_instance" "ec2-mamede-terraform-02" {
-  subnet_id     = aws_subnet.sub-mamede-terraform-1c.id
-  ami= "ami-0e66f5495b4efdd0f"
-  instance_type = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids = ["${aws_security_group.sg-mamede-terraform-01.id}"]
-  key_name="itau-treinamento"
-  root_block_device {
-    encrypted = true
-    volume_size = 8
-  }
-  tags = {
-    Name = "ec2-mamede-terraform-02"
-  }
-}
-resource "aws_instance" "ec2-mamede-terraform-03-private" {
-  subnet_id     = aws_subnet.sub-mamede-terraform-2a-private.id
-  ami= "ami-0e66f5495b4efdd0f"
-  instance_type = "t2.micro"
-  associate_public_ip_address = false
-  vpc_security_group_ids = ["${aws_security_group.sg-mamede-terraform-01.id}"]
-  key_name="itau-treinamento"
-  root_block_device {
-    encrypted = true
-    volume_size = 8
-  }
-  tags = {
-    Name = "ec2-mamede-terraform-03-private"
-  }
-}
+resource "aws_security_group" "sg-g4-terraform-01" {
+  name        = "secgrp-g4-terraform-01"
+  description = "Criado pelo terraform 01"
+  vpc_id = aws_vpc.vpc-g4-terraform-01.id
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    }
+ 
+  ]
 
-# https://www.terraform.io/docs/language/values/outputs.html
-output "instance_private" {
-  value = [
-            "ssh -i ~/aws/key01 ubuntu@${aws_instance.ec2-mamede-terraform-03-private.private_ip}",
-            aws_instance.ec2-mamede-terraform-03-private.public_ip
-          ]
-  description = "Mostra os IPs publicos e privados da maquina criada."
-}
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups  = null,
+      self             = null,
+      description      = "Libera dados da rede interna"
+    }
+  ]
 
-output "instance_puplicas" {
-  value = [
-            "ssh -i ~/aws/key01 ubuntu@${aws_instance.ec2-mamede-terraform-01.public_ip}",
-            "ssh -i ~/aws/key01 ubuntu@${aws_instance.ec2-mamede-terraform-02.public_ip}"
-          ]
-  description = "Mostra os IPs publicos e da maquina criada."
+  tags = {
+    Name = "sg-g4-terraform-01"
+  }
 }
--- 01-terraform_lab_exemplos/11-resource_security_group-01/vpc.tf
+++ 01-terraform_lab_exemplos/11-resource_security_group-01/vpc.tf
@@ -1,62 +1,62 @@
-resource "aws_vpc" "vpc-mamede-terraform-01" {
+resource "aws_vpc" "vpc-g4-terraform-01" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "vpc-mamede-terraform-01"
+    Name = "vpc-g4-terraform-01"
   }
 }
 
-resource "aws_subnet" "sub-mamede-terraform-1a" {
-  vpc_id            = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_subnet" "sub-g4-terraform-1a" {
+  vpc_id            = aws_vpc.vpc-g4-terraform-01.id
   cidr_block        = "10.0.20.0/24"
   availability_zone = "sa-east-1a"
 
   tags = {
-    Name = "sub-mamede-terraform-1a"
+    Name = "sub-g4-terraform-1a"
   }
 }
 
-resource "aws_subnet" "sub-mamede-terraform-1b" {
-  vpc_id            = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_subnet" "sub-g4-terraform-1b" {
+  vpc_id            = aws_vpc.vpc-g4-terraform-01.id
   cidr_block        = "10.0.40.0/24"
   availability_zone = "sa-east-1b"
 
   tags = {
-    Name = "sub-mamede-terraform-1b"
+    Name = "sub-g4-terraform-1b"
   }
 }
 
-resource "aws_subnet" "sub-mamede-terraform-1c" {
-  vpc_id            = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_subnet" "sub-g4-terraform-1c" {
+  vpc_id            = aws_vpc.vpc-g4-terraform-01.id
   cidr_block        = "10.0.60.0/24"
   availability_zone = "sa-east-1c"
 
   tags = {
-    Name = "sub-mamede-terraform-1c"
+    Name = "sub-g4-terraform-1c"
   }
 }
 
-resource "aws_subnet" "sub-mamede-terraform-2a-private" {
-  vpc_id            = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_subnet" "sub-g4-terraform-2a-private" {
+  vpc_id            = aws_vpc.vpc-g4-terraform-01.id
   cidr_block        = "10.0.80.0/24"
   availability_zone = "sa-east-1a"
 
   tags = {
-    Name = "sub-mamede-terraform-2a-private"
+    Name = "sub-g4-terraform-2a-private"
   }
 }
 
 
-resource "aws_internet_gateway" "gtw-mamede-terraform-01" {
-  vpc_id = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_internet_gateway" "gtw-g4-terraform-01" {
+  vpc_id = aws_vpc.vpc-g4-terraform-01.id
 
   tags = {
-    Name = "gtw-mamede-terraform-01"
+    Name = "gtw-g4-terraform-01"
   }
 }
 
-resource "aws_route_table" "rt-mamede-terraform-01" {
-  vpc_id = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_route_table" "rt-g4-terraform-01" {
+  vpc_id = aws_vpc.vpc-g4-terraform-01.id
 
   route = [
       {
@@ -64,7 +64,7 @@ resource "aws_route_table" "rt-mamede-terraform-01" {
         cidr_block                 = "0.0.0.0/0"
         destination_prefix_list_id = ""
         egress_only_gateway_id     = ""
-        gateway_id                 = aws_internet_gateway.gtw-mamede-terraform-01.id
+        gateway_id                 = aws_internet_gateway.gtw-g4-terraform-01.id
         instance_id                = ""
         ipv6_cidr_block            = ""
         local_gateway_id           = ""
@@ -77,37 +77,37 @@ resource "aws_route_table" "rt-mamede-terraform-01" {
   ]
 
   tags = {
-    Name = "rt-mamede-terraform-01"
+    Name = "rt-g4-terraform-01"
   }
 
 }
 
-resource "aws_route_table" "rt-mamede-terraform-02-private" {
-  vpc_id = aws_vpc.vpc-mamede-terraform-01.id
+resource "aws_route_table" "rt-g4-terraform-02-private" {
+  vpc_id = aws_vpc.vpc-g4-terraform-01.id
 
   route = [
       
   ]
   tags = {
-    Name = "rt-mamede-terraform-02-private"
+    Name = "rt-g4-terraform-02-private"
   }
 }
 
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.sub-mamede-terraform-1a.id
-  route_table_id = aws_route_table.rt-mamede-terraform-01.id
+  subnet_id      = aws_subnet.sub-g4-terraform-1a.id
+  route_table_id = aws_route_table.rt-g4-terraform-01.id
 }
 resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.sub-mamede-terraform-1b.id
-  route_table_id = aws_route_table.rt-mamede-terraform-01.id
+  subnet_id      = aws_subnet.sub-g4-terraform-1b.id
+  route_table_id = aws_route_table.rt-g4-terraform-01.id
 }
 resource "aws_route_table_association" "c" {
-  subnet_id      = aws_subnet.sub-mamede-terraform-1c.id
-  route_table_id = aws_route_table.rt-mamede-terraform-01.id
+  subnet_id      = aws_subnet.sub-g4-terraform-1c.id
+  route_table_id = aws_route_table.rt-g4-terraform-01.id
 }
 
 resource "aws_route_table_association" "d" {
-  subnet_id      = aws_subnet.sub-mamede-terraform-2a-private.id
-  route_table_id = aws_route_table.rt-mamede-terraform-02-private.id
+  subnet_id      = aws_subnet.sub-g4-terraform-2a-private.id
+  route_table_id = aws_route_table.rt-g4-terraform-02-private.id
 }
