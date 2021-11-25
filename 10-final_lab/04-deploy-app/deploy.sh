9ff8f92ca00298c41a374d5bfb43fc7eb3b68e44
root <root@Juliano.localdomain>
2021-11-25 16:04:04 -0300

    vai

--- 10-final_lab/04-deploy-app/deploy.sh
+++ 10-final_lab/04-deploy-app/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd 10-final_lab/04-deploy-app
 ANSIBLE_OUT=$(ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa)
 echo $ANSIBLE_OUT
